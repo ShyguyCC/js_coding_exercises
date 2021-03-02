c2ccf3989371fa9ae9f30951b266d8b5929f13a4
Ciaran <ShyguyCC@users.noreply.github.com>
2021-03-02 13:51:02 +0000

    finished generate initials and add vat functions in exersize 1

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -6,14 +6,22 @@ function capitalize(word) {
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
-  if (lastName === undefined) throw new Error("lastName is required");
+    if (lastName === undefined) throw new Error("lastName is required");
   // Add your code here!
+    //adds on capitalize funtion, just return the first letters upper cased and . between them.
+    return firstName.substring(0, 1).toUpperCase() + "." + lastName.substring(0, 1);
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
   // Add your code here!
+    //here we get the original price and add the vat rate by diving it by 100 and multiplying with the price ie 45% * 100 = .45 * 100 = 45. then 45 + 100 = 45.
+    //then we use math round and number epsilon to round up any float values to 2 decimals.
+    var temp = originalPrice + ((vatRate / 100) * originalPrice);
+    temp = Math.round((temp + Number.EPSILON) * 100) / 100;
+    console.log(temp);
+    return temp;
 }
 
 function getSalePrice(originalPrice, reduction) {
